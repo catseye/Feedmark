ed5aacf515b168500d735602caa07f48038efcbe
Chris Pressey <cpressey@catseye.tc>
2017-07-20 13:33:39 +0100

    Always read the input files. Only output Atom if requested.

--- src/feedmark/main.py
+++ src/feedmark/main.py
@@ -9,12 +9,8 @@ import markdown
 from feedmark.parser import Parser
 
 
-def extract_entries(markdown_text, properties):
-    """`properties` should be a dict-like object; it will be populated by the properties
-    of the set of entries found in the document."""
-
-    parser = Parser(markdown_text)
-    document = parser.parse_document()
+def extract_feed_entries(document):
+    properties = {}
 
     properties['title'] = document.title
     properties['author'] = document.properties['author']
@@ -34,17 +30,15 @@ def extract_entries(markdown_text, properties):
                               summary=summary, links=links)
         entries.append(entry)
 
-    return entries
+    return entries, properties
 
 
-def feedmark_atomize(in_filenames, out_filename, limit=None):
+def feedmark_atomize(documents, out_filename, limit=None):
     entries = []
     properties = {}
-    for in_filename in in_filenames:
-        with codecs.open(in_filename, 'r', encoding='utf-8') as f:
-            markdown_text = f.read()
-        these_properties = {}
-        these_entries = extract_entries(markdown_text, these_properties)
+
+    for document in documents:
+        these_entries, these_properties = extract_feed_entries(document)
         properties.update(these_properties)  # TODO: something more elegant than this
         entries.extend(these_entries)
 
@@ -68,7 +62,7 @@ def feedmark_atomize(in_filenames, out_filename, limit=None):
 def main(args):
     argparser = ArgumentParser()
 
-    argparser.add_argument('infiles', nargs='+', metavar='FILENAME', type=str,
+    argparser.add_argument('input_files', nargs='+', metavar='FILENAME', type=str,
         help='Markdown files containing the embedded entries'
     )
     argparser.add_argument('--output-atom', metavar='FILENAME', type=str,
@@ -77,8 +71,17 @@ def main(args):
 
     options = argparser.parse_args(sys.argv[1:])
 
+    documents = []
+
+    for filename in options.input_files:
+        with codecs.open(filename, 'r', encoding='utf-8') as f:
+            markdown_text = f.read()
+        parser = Parser(markdown_text)
+        document = parser.parse_document()
+        documents.append(document)
+
     if options.output_atom:
-        feedmark_atomize(options.infiles, options.output_atom)
+        feedmark_atomize(documents, options.output_atom)
 
 
 if __name__ == '__main__':
