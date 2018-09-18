5aef44e0d2ab09dfa43b2f17f68bffdc2db6721e
Chris Pressey <cpressey@catseye.tc>
2018-09-18 19:06:36 +0100

    Make sure to fail if it fails.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-PYTHONPATH=src python2 src/feedmark/tests.py
-PYTHONPATH=src python3 src/feedmark/tests.py
+PYTHONPATH=src python2 src/feedmark/tests.py || exit 1
+PYTHONPATH=src python3 src/feedmark/tests.py || exit 1
