a0669c75a4f101282e7ebd7edd7f3ff1231e88ca
Chris Pressey <cpressey@catseye.tc>
2019-05-02 15:15:10 +0100

    Add setup.py (somewhat experimental, is not official package yet.)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='Feedmark',
+    version='0.9',
+    description='Curationally-formatted subset of Markdown and tools for processing it',
+    author='Chris Pressey',
+    author_email='packages@catseye.tc',
+    packages=['feedmark'],
+)
