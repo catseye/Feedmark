faf47b289494e693010eb1c7970147fca72d91f3
Chris Pressey <cpressey@catseye.tc>
2019-09-06 11:24:28 +0100

    Add URL to setup.py.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 setup(
     name='Feedmark',
     version='0.9',
-    description='Curationally-formatted subset of Markdown and tools for processing it',
+    description='Tools for processing documents in Feedmark, a curation-oriented subset of Markdown',
     author='Chris Pressey',
     author_email='packages@catseye.tc',
+    url='https://catseye.tc/node/Feedmark',
     packages=['feedmark'],
     package_dir={'': 'src'},
     scripts=['bin/feedmark'],
